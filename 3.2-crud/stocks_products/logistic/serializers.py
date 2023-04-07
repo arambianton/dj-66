b55f0d2e261e755d5be622d267096ee6b201e028
Anton <arambianton@gmail.com>
2023-04-07 16:12:33 +0300

    new

--- 3.2-crud/stocks_products/logistic/serializers.py
+++ 3.2-crud/stocks_products/logistic/serializers.py
@@ -16,37 +16,21 @@ class ProductPositionSerializer(serializers.ModelSerializer):
 
 
 class StockSerializer(serializers.ModelSerializer):
-
     positions = ProductPositionSerializer(
-        many=True)
+        many=True, read_only=True)
     
 
     class Meta:
         model = Stock
         fields = ['address', 'positions']
-        read_only_fields = ('stock',)
     
     # positions = ProductPositionSerializer(many=True)
     # настройте сериализатор для склада 
     def create(self, validated_data):
-        positions = validated_data.pop('positions')
         stock = super().create(validated_data)
+        positions = validated_data.pop('positions')
         for position in positions:
             StockProduct.objects.update_or_create(stock=stock, **position)
         return stock
             
-    def update(self, instance, validated_data):
-        # достаем связанные данные для других таблиц
-        # positions = validated_data.pop('positions')
-
-        # обновляем склад по его параметрам
-        # stock = super().update(instance, validated_data)
-        stock = super().update(instance, validated_data)
-        # for position in positions:
-        #     StockProduct.objects.update_or_create(stock=stock, **position)
-
-        # здесь вам надо обновить связанные таблицы
-        # в нашем случае: таблицу StockProduct
-        # с помощью списка positions
-
-        return stock
+#    def update(self, instance, validated_data):
